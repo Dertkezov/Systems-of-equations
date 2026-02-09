46f3e8f203c3d9f72931a3a477f9ee60509bcf14
Victor Dertkezov <141644764+Dertkezov@users.noreply.github.com>
2026-02-09 11:30:45 +0300

    Update main.cpp

--- 1/main.cpp
+++ 1/main.cpp
@@ -7,60 +7,62 @@ int main(){
     test1();
     test2();
 
-    std::cout << "Enter sistem size:" << std::endl;
-    int N;
-    std::cin >> N;
+    while (true){
+        std::cout << "Enter sistem size:" << std::endl;
+        int N;
+        std::cin >> N;
 
-    std::vector<std::vector<double>> A(N, std::vector<double>(N, 0.0));
-    std::vector<double> b(N, 0.0);
+        std::vector<std::vector<double>> A(3, std::vector<double>(N, 0.0));
+        std::vector<double> b(N, 0.0);
 
-    std::cout << "Enter matrix. It has to fit condition that |b_i| >= |a_i| + |c_i|:" << std::endl;
+        std::cout << "Enter matrix. It has to fit condition that |b_i| >= |a_i| + |c_i|:" << std::endl;
 
-    bool k = false;
-    while (k == false){
-        std::cout << "Enter lower diagonal (" << N - 1 << " numbers):" << std::endl;
-        for (int i = 1; i < N; i++) {
-            double a_k;
-            std::cin >> a_k;
-            A[i][i - 1] = a_k;
+        bool k = false;
+        while (k == false){
+            std::cout << "Enter lower diagonal (" << N - 1 << " numbers):" << std::endl;
+            for (int i = 1; i < N; i++) {
+                double a_k;
+                std::cin >> a_k;
+                A[0][i] = a_k;
+            }
+
+            std::cout << "Enter main diagonal (" << N << " numbers):" << std::endl;
+            for (int i = 0; i < N; i++) {
+                double a_k;
+                std::cin >> a_k;
+                A[1][i] = a_k;
+            }
+
+            std::cout << "Enter upper diagonal (" << N - 1 << " numbers):" << std::endl;
+            for (int i = 0; i < N - 1; i++) {
+                double a_k;
+                std::cin >> a_k;
+                A[2][i] = a_k;
+            }
+
+            if (matrix_check(A, N)){
+                std::cout << "Matrix is correct!" << std::endl;
+                k = true;
+            }
+            else{
+                std::cout << "Matrix is incorrect, enter again!!!" << std::endl;
+                k = false;
+            }
         }
+        
 
-        std::cout << "Enter main diagonal (" << N << " numbers):" << std::endl;
+        std::cout << "Enter column of free members (" << N << "numbers):" << std::endl;
         for (int i = 0; i < N; i++) {
-            double a_k;
-            std::cin >> a_k;
-            A[i][i] = a_k;
+            double b_k;
+            std::cin >> b_k;
+            b[i] = b_k;
         }
 
-        std::cout << "Enter lower diagonal (" << N - 1 << " numbers):" << std::endl;
-        for (int i = 1; i < N; i++) {
-            double a_k;
-            std::cin >> a_k;
-            A[i - 1][i] = a_k;
+        std::vector<double> x = x_solution(A, b, N);
+        
+        std::cout << "Result:" << std::endl;
+        for (int i = 0; i < N; i++) {
+            std::cout << "x_"<< i << " = " << x[i] << std::endl;
         }
-
-        if (matrix_check(A)){
-            std::cout << "Matrix is correct!" << std::endl;
-            k = true;
-        }
-        else{
-            std::cout << "Matrix is incorrect, enter again!!!" << std::endl;
-            k = false;
-        }
-    }
-    
-
-    std::cout << "Enter column of free members (" << N << "numbers):" << std::endl;
-    for (int i = 0; i < N; i++) {
-        double b_k;
-        std::cin >> b_k;
-        b[i] = b_k;
-    }
-
-    std::vector<double> x = x_solution(A, b);
-    
-    std::cout << "Result:" << std::endl;
-    for (int i = 0; i < N; i++) {
-        std::cout << "x_"<< i << " = " << x[i] << std::endl;
     }
 }
