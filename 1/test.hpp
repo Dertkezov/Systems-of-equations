c404cb3a1127d429e80fe8f07f30898da10217b3
Victor Dertkezov <141644764+Dertkezov@users.noreply.github.com>
2026-02-09 11:31:59 +0300

    Update test.hpp

--- 1/test.hpp
+++ 1/test.hpp
@@ -6,54 +6,54 @@
 void test1 (){
     int N = 3;
 
-    std::vector<std::vector<double>> A(N, std::vector<double>(N, 0.0));
+    std::vector<std::vector<double>> A(3, std::vector<double>(N, 0.0));
     std::vector<double> b(N, 0.0);
     
     for (int i = 1; i < N; i++) {
-        A[i][i - 1] = 1;
+        A[0][i] = 1;
     }
 
     for (int i = 0; i < N; i++) {
-        A[i][i] = 5;
+        A[1][i] = 5;
     }
 
-    for (int i = 1; i < N; i++) {
-        A[i - 1][i] = 2;
+    for (int i = 0; i < N - 1; i++) {
+        A[2][i] = 2;
     }
 
     b[0] = 9;
     b[1] = 17;
     b[2] = 17;
     
-    std::vector<double> x = x_solution(A, b);
+    std::vector<double> x = x_solution(A, b, N);
 
     double d = 1e-9;
     if (((1 - d <= x[0]) && (x[0] <= 1 + d)) && 
     ((2 - d <= x[1]) && (x[1] <= 2 + d)) && 
     ((3 - d <= x[2]) && (x[2] <= 3 + d))){
-        std::cout << "Test for matrix 3 * 3 is correct!" << std::endl;
+        std::cout << "Test of matrix 3 * 3 is correct!" << std::endl;
     }
     else{
-        std::cout << "Incorrect, check solver.cpp and main.cpp!!!" << std::endl;
+        std::cout << "Test of matrix 3 * 3 is incorrect, check solver.cpp and main.cpp!!!" << std::endl;
     }
 }
 
 void test2 (){
     int N = 5;
 
-    std::vector<std::vector<double>> A(N, std::vector<double>(N, 0.0));
+    std::vector<std::vector<double>> A(3, std::vector<double>(N, 0.0));
     std::vector<double> b(N, 0.0);
     
     for (int i = 1; i < N; i++) {
-        A[i][i - 1] = 1;
+        A[0][i] = 1;
     }
 
     for (int i = 0; i < N; i++) {
-        A[i][i] = 5;
+        A[1][i] = 5;
     }
 
-    for (int i = 1; i < N; i++) {
-        A[i - 1][i] = 2;
+    for (int i = 0; i < N - 1; i++) {
+        A[2][i] = 2;
     }
 
     b[0] = 9;
@@ -62,33 +62,27 @@ void test2 (){
     b[3] = 33;
     b[4] = 29;
     
-    std::vector<double> x = x_solution(A, b);
+    std::vector<double> x = x_solution(A, b, N);
 
     double d = 1e-9;
-    if (((1 - d <= x[0]) & (x[0] <= 1 + d)) && 
+    if (((1 - d <= x[0]) && (x[0] <= 1 + d)) && 
     ((2 - d <= x[1]) && (x[1] <= 2 + d)) && 
     ((3 - d <= x[2]) && (x[2] <= 3 + d)) && 
     ((4 - d <= x[3]) && (x[3] <= 4 + d)) && 
     ((5 - d <= x[4]) && (x[4] <= 5 + d))){
-        std::cout << "Test for matrix 5 * 5 is correct!" << std::endl;
+        std::cout << "Test of matrix 5 * 5 is correct!" << std::endl;
     }
     else{
-        std::cout << "Incorrect, check solver.cpp and main.cpp!!!" << std::endl;
+        std::cout << "Test of matrix 5 * 5 is incorrect, check solver.cpp and main.cpp!!!" << std::endl;
     }
 }
 
-bool matrix_check(std::vector<std::vector<double>>& A){
-    int N = A.size();
+bool matrix_check(std::vector<std::vector<double>>& A, int N){
     std::cout << "Check of |b_i| > |a_i| + |c_i|:" << std::endl;
-    if ((std::abs(A[0][0]) > std::abs(A[0][1])) && (std::abs(A[N - 1][N - 1]) > std::abs(A[N - 1][N - 2]))){
-        for (int i = 1; i < N - 1; i++){
-            if (std::abs(A[i][i]) <= (std::abs(A[i][i - 1]) + std::abs(A[i][i + 1]))){
-                return(false);
-            }
+    for (int i = 0; i < N; i++){
+        if (std::abs(A[1][i]) <= (std::abs(A[0][i]) + std::abs(A[2][i]))){
+            return(false);
         }
-    }
-    else{
-        return(false);
     }
     return(true);
 }
