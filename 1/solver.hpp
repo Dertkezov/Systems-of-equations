65ea22237b6b72626be1410f6e84d85d77c18017
Victor Dertkezov <141644764+Dertkezov@users.noreply.github.com>
2026-02-09 11:31:29 +0300

    Update solver.hpp

--- 1/solver.hpp
+++ 1/solver.hpp
@@ -2,24 +2,23 @@
 #include <vector>
 #include <iostream>
 
-inline std::vector<double> x_solution(std::vector<std::vector<double>>& A, std::vector<double>& b){
-    int N = A.size();
+inline std::vector<double> x_solution(std::vector<std::vector<double>>& A, std::vector<double>& b, int N){
     std::vector<double> x(N, 0.0);
-    std::vector<double> p(N - 1, 0.0);
-    std::vector<double> q(N - 1, 0.0);
+    std::vector<double> p(N, 0.0);
+    std::vector<double> q(N, 0.0);
 
-    p[0] = -A[0][1] / A[0][0];
-    q[0] = b[0] / A[0][0];
+    p[1] = -A[2][0] / A[1][0];
+    q[1] = b[0] / A[1][0];
 
     for (int i = 1; i < N - 1; i++) {
-        p[i] = -A[i][i + 1] / (A[i][i] + A[i][i - 1] * p[i - 1]);
-        q[i] = (b[i] - A[i][i - 1] * q[i - 1]) / (A[i][i] + A[i][i - 1] * p[i - 1]);
+        p[i + 1] = -A[2][i] / (A[1][i] + A[0][i] * p[i]);
+        q[i + 1] = (b[i] - A[0][i] * q[i]) / (A[1][i] + A[0][i] * p[i]);
     }
 
-    x[N - 1] = (b[N - 1] - A[N - 1][N - 2] * q[N - 2]) / (A[N - 1][N - 1] + A[N - 1][N - 2] * p[N - 2]);
+    x[N - 1] = (b[N - 1] - A[0][N - 1] * q[N - 1]) / (A[1][N - 1] + A[0][N - 1] * p[N - 1]);
 
-    for (int i = N - 2; i >= 0; i--) {
-        x[i] = p[i] * x[i + 1] + q[i];
+    for (int i = N - 1; i >= 1; i--) {
+        x[i - 1] = p[i] * x[i] + q[i];
     }
 
     return (x);
