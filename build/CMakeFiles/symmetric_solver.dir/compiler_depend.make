36e66d5263caeccfc7c841114a31e467285d3bdf
Dertkezov <vkdert131096@gmail.com>
2026-04-03 21:18:17 +0300

    remove build

--- build/CMakeFiles/symmetric_solver.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for symmetric_solver.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/eque/HW
-# Build directory: /mnt/d/eque/HW/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
